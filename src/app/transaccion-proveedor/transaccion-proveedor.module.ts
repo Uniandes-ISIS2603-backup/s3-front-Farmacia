6a8fabfb45dd7d95521247547e39994611c436ca
Enguenye <jd.florezg1@uniandes.edu.co>
2018-10-18 10:07:23 -0500

    arreglo

--- src/app/transaccion-proveedor/transaccion-proveedor.module.ts
+++ src/app/transaccion-proveedor/transaccion-proveedor.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TransaccionProveedorListComponent } from '../transaccionProveedor/transaccion-proveedor-list/transaccion-proveedor-list.component';
+import { TransaccionProveedorListComponent } from './transaccion-proveedor-list/transaccion-proveedor-list.component';
 
 @NgModule({
   imports: [
