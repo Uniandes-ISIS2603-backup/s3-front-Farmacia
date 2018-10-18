b83cf6fbaec41eb79ed95ad016ac72f39d923b89
afrojasp <af.rojasp@uniandes.edu.co>
2018-10-18 10:10:03 -0500

    Modulo basico de cliente

---
+++ src/app/cliente/cliente.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClienteListComponent } from './cliente-list/cliente-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ClienteListComponent]
+})
+export class ClienteModule { }
