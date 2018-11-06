2c72e11e0d589d107ac8ac83c2c3db3ba0d3fe55
jupardo <jd.pardo@uniandes.edu.co>
2018-11-06 00:56:16 -0500

    created producto-create component

--- src/app/producto/producto.module.ts
+++ src/app/producto/producto.module.ts
@@ -4,13 +4,14 @@ import { ProductoListComponent } from './producto-list/producto-list.component';
 import { ProductoService } from './producto.service';
 import { ProductoDetailComponent } from './producto-detail/producto-detail.component';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
+import { ProductoCreateComponent } from './producto-create/producto-create.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AppRoutingModule
   ],
-  declarations: [ProductoListComponent, ProductoDetailComponent],
+  declarations: [ProductoListComponent, ProductoDetailComponent, ProductoCreateComponent],
   providers: [ProductoService]
 })
 export class ProductoModule { }
