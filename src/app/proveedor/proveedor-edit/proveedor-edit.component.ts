94754e00f0b5193d2b8b9133326cc0182aa854d7
fj.gonzalez <fj.gonzalez@uniandes.edu.co>
2018-11-21 11:45:13 -0500

    Proveedor edit y mejorada la visualizacion. Ahora modo tabla

--- src/app/proveedor/proveedor-edit/proveedor-edit.component.ts
+++ src/app/proveedor/proveedor-edit/proveedor-edit.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Output, EventEmitter, Input} from '@angular/core';
 import { ProveedorService } from '../proveedor.service';
 import { ProductoService } from 'src/app/producto/producto.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Producto } from 'src/app/producto/producto';
+import {TransaccionProveedor} from '../transaccion-proveedor';
+import { Proveedor } from '../proveedor';
+import { ProveedorDetail } from '../proveedor-detail';
 
 @Component({
   selector: 'app-proveedor-edit',
@@ -19,7 +23,40 @@ export class ProveedorEditComponent implements OnInit {
     private route : ActivatedRoute
   ) { }
 
-  ngOnInit() {
+  @Input() proveedor : Proveedor;
+
+   /**
+    * The output which tells the parent component
+    * that the user no longer wants to edit a provider
+    */
+   @Output() cancel = new EventEmitter();
+
+   /**
+    * The output which tells the parent component
+    * that the user updated the provider's information
+    */
+   @Output() update = new EventEmitter();
+
+  guardarCambios() {
+    this.proveedorService.updateProveedor(this.proveedor)
+    .subscribe(() => {
+      this.toastrService.success('Se guardaron los cambios del proveedor');
+    }, error => {
+      this.toastrService.error(error, "Error")
+      this.update.emit();
+    });
+  }
+
+  cancelEdition(): void {
+    this.toastrService.warning('The provider wasn\'t edited', 'Provider edition');
+    this.router.navigate(['/proveedores/list']);
+    this.cancel.emit();
+}
+
+
+  ngOnInit() 
+  {
+    this.proveedor = new ProveedorDetail();
   }
 
 }
