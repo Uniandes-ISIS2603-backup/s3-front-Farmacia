03460a4af3d308b364f58c8aed341c4ddb5be0a1
fj.gonzalez <fj.gonzalez@uniandes.edu.co>
2018-11-24 22:37:31 -0500

    Modal añadido, buscar hacerlo funcionar

--- src/app/proveedor/proveedor.service.ts
+++ src/app/proveedor/proveedor.service.ts
@@ -5,7 +5,8 @@ import { TransaccionProveedor } from '../proveedor/transaccion-proveedor';
 import { Proveedor } from './proveedor';
 import { environment } from '../../environments/environment';
 import { ProveedorDetail } from './proveedor-detail';
-import {TransaccionProveedorDetail} from '../proveedor/transaccion-proveedor-detail';
+import { TransaccionProveedorDetail } from '../proveedor/transaccion-proveedor-detail';
+import { ProductoDetail } from '../producto/producto-detail';
 /**
 * Ruta que maneja la conexion con el back.
 */
@@ -19,7 +20,7 @@ const proveedores = '/proveedores';
 @Injectable()
 export class ProveedorService {
 
-    
+
     /**
     * Constructor del servicio.
     * @param http The HttpClient 
@@ -48,7 +49,7 @@ export class ProveedorService {
         return this.http.post<Proveedor>(API_URL + proveedores, proveedor);
     }
 
-   public getTransaccionesProveedor(idProveedor): Observable<TransaccionProveedor[]> {
+    public getTransaccionesProveedor(idProveedor): Observable<TransaccionProveedor[]> {
         return this.http.get<TransaccionProveedor[]>(API_URL + proveedores + '/' + idProveedor + transaccionesProveedor);
     }
 
@@ -65,21 +66,27 @@ export class ProveedorService {
     * @param transaccionProveedor La transaccion que sera creada
     * @returns Confirmacion de la creacion de la transaccion
     */
-   public createTransaccionProveedor(transaccionProveedor, idProveedor): Observable<TransaccionProveedorDetail> {
-    return this.http.post<TransaccionProveedorDetail>(API_URL + proveedores + '/' + idProveedor + transaccionProveedor, transaccionProveedor);
-   }
-   /**
-    * Edita la informacion de un proveedor
-    * @param proveedor 
-    * @returns Confirmacion de la actualizacion del proveedor
-    */
-   updateProveedor(proveedor): Observable<ProveedorDetail>{
-       return this.http.put<ProveedorDetail>(API_URL + proveedores + '/' + proveedor.id , proveedor);
-   } 
-   /**
-    * 
-    */
-   deleteProveedor (proveedorId): Observable<boolean> {
-    return this.http.delete<boolean>(API_URL + proveedores + '/' + proveedorId);
-}
+    public createTransaccionProveedor(transaccionProveedor, idProveedor): Observable<TransaccionProveedorDetail> {
+        return this.http.post<TransaccionProveedorDetail>(API_URL + proveedores + '/' + idProveedor + transaccionProveedor, transaccionProveedor);
+    }
+    /**
+     * Edita la informacion de un proveedor
+     * @param proveedor 
+     * @returns Confirmacion de la actualizacion del proveedor
+     */
+    updateProveedor(proveedor): Observable<ProveedorDetail> {
+        return this.http.put<ProveedorDetail>(API_URL + proveedores + '/' + proveedor.id, proveedor);
+    }
+    /**
+     * 
+     */
+    deleteProveedor(proveedorId): Observable<boolean> {
+        return this.http.delete<boolean>(API_URL + proveedores + '/' + proveedorId);
+    }
+    /**
+     * 
+     */
+    asociateProducto(proveedorId, productoId): void{
+         this.http.post<ProductoDetail>(API_URL + proveedores + '/'+ proveedorId + '/productos', productoId);
+    }
 }
