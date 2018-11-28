7a976e4665ebe5147cc9f4817b1251e0b12e6c5b
Francisco <fj.gonzalez@uniandes.edu.co>
2018-11-28 15:31:35 -0500

    Ya realizado el tema de proveedor

--- src/app/proveedor/proveedor-list/proveedor-list.component.ts
+++ src/app/proveedor/proveedor-list/proveedor-list.component.ts
@@ -1,148 +1,200 @@
-import { Component, OnInit , ViewContainerRef} from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { ProveedorService } from '../proveedor.service';
 import { Proveedor } from '../proveedor';
 import { ProveedorDetail } from '../proveedor-detail';
-import {ToastrService} from 'ngx-toastr';
-import {ModalDialogService, SimpleModalComponent} from 'ngx-modal-dialog';
+import { ToastrService } from 'ngx-toastr';
+import { ModalDialogService, SimpleModalComponent } from 'ngx-modal-dialog';
 /**
 * El componente listar del proveedor.
 */
 @Component({
-  selector: 'app-proveedor',
-  templateUrl: './proveedor-list.component.html',
-  styleUrls: ['./proveedor-list.component.scss']
+    selector: 'app-proveedor',
+    templateUrl: './proveedor-list.component.html',
+    styleUrls: ['./proveedor-list.component.scss']
 })
 export class ProveedorListComponent implements OnInit {
 
-  showCreate: boolean;
+    showCreate: boolean;
 
-  showEdit: boolean;
+    showEdit: boolean;
 
-  selectedProveedor: Proveedor;
+    selectedProveedor: Proveedor;
 
-  /**
-  * Constructor for the providerlist
-  * @param proveedorService The provider's services supplier
-  */
-  constructor(
-      private proveedorService: ProveedorService,
-      private modalDialogService: ModalDialogService,
-      private viewRef: ViewContainerRef,
-      private toastrService: ToastrService
+    proveedorACrear: Proveedor;
+
+
+    habilitarCreate: boolean;
+
+    habilitarEdit: boolean;
+
+    /**
+    * Constructor for the providerlist
+    * @param proveedorService The provider's services supplier
+    */
+    constructor(
+        private proveedorService: ProveedorService,
+        private modalDialogService: ModalDialogService,
+        private viewRef: ViewContainerRef,
+        private toastrService: ToastrService
     ) { }
 
-  /**
-  * The list of providers
-  */
-  proveedores: Proveedor[];
+    /**
+    * The list of providers
+    */
+    proveedores: Proveedor[];
 
-  /**
-   * The list of filtered providers
-   */
-  filtered: Proveedor[];
+    /**
+     * The list of filtered providers
+     */
+    filtered: Proveedor[];
 
-  /**
-  * The id of the provider to create
-  */
-  proveedor_id: number;
+    /**
+    * The id of the provider to create
+    */
+    proveedor_id: number;
 
 
-  onSelected(id: number): void {
-    this.showCreate = false;
-    this.showEdit = false;
-    this.proveedor_id = id;
-    this.selectedProveedor = new ProveedorDetail();
-    this.getProveedorDetail();
-}
-
-/**
-* Shows or hides the create component
-*/
-showHideCreate(): void {
-    this.showEdit = false;
-    this.showCreate = !this.showCreate;
-}
-
-/**
-* Shows or hides the create component
-*/
-showHideEdit(proveedor_id: number): void {
-    if (!this.showEdit || (this.showEdit && proveedor_id !== this.selectedProveedor.id)) {
+    onSelected(id: number): void {
         this.showCreate = false;
-        this.showEdit = true;
-        this.proveedor_id = proveedor_id;
+        this.showEdit = false;
+        this.proveedor_id = id;
         this.selectedProveedor = new ProveedorDetail();
         this.getProveedorDetail();
-    } else {
+    }
+
+    /**
+    * Shows or hides the create component
+    */
+    showHideCreate(): void {
+        this.showEdit = false;
+        this.showCreate = !this.showCreate;
+    }
+
+    /**
+    * Shows or hides the create component
+    */
+    showHideEdit(proveedor_id: number): void {
+        if (!this.showEdit || (this.showEdit && proveedor_id !== this.selectedProveedor.id)) {
+            this.showCreate = false;
+            this.showEdit = true;
+            this.proveedor_id = proveedor_id;
+            this.selectedProveedor = new ProveedorDetail();
+            this.getProveedorDetail();
+        } else {
+            this.showEdit = false;
+        }
+    }
+
+    /**
+    * Asks the service to update the list of authors
+    */
+    getProveedores(): void {
+        this.proveedorService.getProveedores()
+            .subscribe(proveedores => {
+                this.proveedores = proveedores;
+                this.filtered = this.proveedores;
+            });
+    }
+
+    getProveedorDetail(): void {
+        this.proveedorService.getProveedorDetail(this.proveedor_id)
+            .subscribe(selectedProveedor => {
+                this.selectedProveedor = selectedProveedor;
+            });
+    }
+
+    updateProveedor(): void {
+
         this.showEdit = false;
     }
-}
-
-/**
-* Asks the service to update the list of authors
-*/
-getProveedores(): void {
-    this.proveedorService.getProveedores()
-        .subscribe(proveedores => {
-            this.proveedores = proveedores;
-            this.filtered = this.proveedores;
+    deleteProveedor(proveedorId): void {
+        this.modalDialogService.openDialog(this.viewRef, {
+            title: 'Eliminar un proveedor.',
+            childComponent: SimpleModalComponent,
+            data: { text: '¿Está seguro de eliminar a este proveedor? Recuede que esta acción es irreversible' },
+            actionButtons: [
+                {
+                    text: 'Yes',
+                    buttonClass: 'btn btn-success',
+                    onAction: () => {
+                        this.proveedorService.deleteProveedor(proveedorId).subscribe(() => {
+                            this.toastrService.success('El proveedor fue eliminado satisfactoriamente.', 'Proveedor eliminado');
+                            this.ngOnInit();
+                        }, err => {
+                            this.toastrService.error(err, 'Error');
+                        });
+                        return true;
+                    }
+                },
+                { text: 'No', onAction: () => true }
+            ]
         });
-}
-
-getProveedorDetail(): void {
-    this.proveedorService.getProveedorDetail(this.proveedor_id)
-        .subscribe(selectedProveedor => {
-            this.selectedProveedor = selectedProveedor;
-        });
-}
-
-updateProveedor(): void {
-    this.showEdit = false;
-}
-
-deleteProveedor(proveedorId): void {
-    this.modalDialogService.openDialog(this.viewRef, {
-        title: 'Eliminar un proveedor.',
-        childComponent: SimpleModalComponent,
-        data: {text: '¿Está seguro de eliminar a este proveedor? Recuede que esta acción es irreversible'},
-        actionButtons: [
-            {
-                text: 'Yes',
-                buttonClass: 'btn btn-success',
-                onAction: () => {
-                    this.proveedorService.deleteProveedor(proveedorId).subscribe(() => {
-                        this.toastrService.success('El proveedor fue eliminado satisfactoriamente.', 'Proveedor eliminado');
-                        this.ngOnInit();
-                    }, err => {
-                        this.toastrService.error(err, 'Error');
-                    });
-                    return true;
-                }
-            },
-            {text: 'No', onAction: () => true}
-        ]
-    });
-    this.ngOnInit();
-}
+        this.ngOnInit();
+    }
 
 
-/**
-* This will initialize the component by retrieving the list of authors from the service
-* This method will be called when the component is created
-*/
-ngOnInit() {
-    this.showCreate = false;
-    this.showEdit = false;
-    this.selectedProveedor = undefined;
-    this.proveedor_id = undefined;
-    this.getProveedores();
-}
+    /**
+    * This will initialize the component by retrieving the list of authors from the service
+    * This method will be called when the component is created
+    */
+    ngOnInit() {
+        this.showCreate = false;
+        this.showEdit = false;
+        this.selectedProveedor = undefined;
+        this.proveedorACrear = undefined;
+        this.proveedor_id = undefined;
+        this.habilitarCreate = false;
+        this.habilitarEdit = false;
+        this.getProveedores();
+    }
 
-filter(input) {
-    this.filtered = this.proveedores.filter(proveedor =>
-        proveedor.nombre.toUpperCase().includes(input.toUpperCase()) ||
-        proveedor.id.toString().includes(input));
-    console.log(input);
-}
+    filter(input) {
+        this.filtered = this.proveedores.filter(proveedor =>
+            proveedor.nombre.toUpperCase().includes(input.toUpperCase()) ||
+            proveedor.id.toString().includes(input));
+        console.log(input);
+    }
+
+    saveName(event) {
+        this.selectedProveedor.nombre = event.target.outerText;
+        this.habilitarEdit = true;
+
+    }
+    saveNameCreate(event) {
+        this.proveedorACrear = new ProveedorDetail();
+        this.proveedorACrear.nombre = event.target.outerText;
+        this.habilitarCreate = true;
+
+    }
+
+    edit(proveedorId) {
+        if (this.habilitarEdit) {
+            this.selectedProveedor.id = proveedorId;
+            this.proveedorService.updateProveedor(this.selectedProveedor).subscribe(() => {
+                this.toastrService.success('Se guardaron los cambios del proveedor');
+                this.ngOnInit();
+            }, error => {
+                this.toastrService.error(error, "Error")
+            });
+        }
+        else {
+            this.toastrService.info("Ups, ¡es el mismo nombre!")
+        }
+    }
+    create() {
+        if (this.habilitarCreate) {
+            this.proveedorService.createProveedor(this.proveedorACrear).subscribe(() => {
+                this.toastrService.success('Se creo al proveedor');
+                this.ngOnInit();
+                this.showCreate = true;
+            }, error => {
+                this.toastrService.error(error, "No se puedo agregar")
+            });
+        }
+        else {
+            this.toastrService.info("Primero escriba el nombre del proveedor para poder crearlo.")
+        }
+    }
+
 
 }
